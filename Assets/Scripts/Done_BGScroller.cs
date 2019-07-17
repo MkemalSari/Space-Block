5219202210b9850aa3b76dadc085112184a5d811
mustafa sarı <kemal_sari_267@hotmail.com>
2019-07-17 10:46:05 +0300

    v2

---
+++ Assets/Scripts/Done_BGScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_BGScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeY;
+
+	private Vector2 startPosition;
+
+	void Start ()
+	{
+		startPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
+		transform.position = startPosition + Vector2.down * newPosition;
+	}
+}
